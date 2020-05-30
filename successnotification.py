497ba7f51cd6dd00d6dcc7ea02028205615182d5
Minshu123 <47297558+Minshu123@users.noreply.github.com>
2020-05-30 14:49:56 +0530

    firstcommit

--- successnotification.py
+++ successnotification.py
@@ -1,1 +1,11 @@
 import smtplib
+sender_email="minshudwarika@gmail.com"
+rec_email="minshunitrkl@gmail.com"
+password="resonance"
+message="hey ,i am checking whether you are receiving message or not "
+server=smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+server.login(sender_email,password)
+print("Login success")
+server.sendmail(sender_email,rec_email,message)
+print("Email  has been sent")
