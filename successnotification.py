3acbfabce2948a5d3bd3392802b12b105b05c946
Minshu123 <47297558+Minshu123@users.noreply.github.com>
2020-05-30 14:54:59 +0530

    second commit

--- successnotification.py
+++ successnotification.py
@@ -3,7 +3,7 @@ sender_email="minshudwarika@gmail.com"
 rec_email="minshunitrkl@gmail.com"
 password="resonance"
 message="hey ,i am checking whether you are receiving message or not "
-server=smtplib.SMTP("smtp.gmail.com",587)
+server=smtplib.SMTP("smtp.gmail.com",587) 
 server.starttls()
 server.login(sender_email,password)
 print("Login success")
